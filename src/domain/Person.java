b1fc7e651b4ae9fd637dcbdae667dd24ecfb0f68
Khaia <hkarur@asu.edu>
2022-10-17 18:14:14 -0700

    Fourth commit

--- src/domain/Person.java
+++ src/domain/Person.java
@@ -1,5 +1,40 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import test.ProductList;
+
 public abstract class Person {
+    public String name;
+    int type;
+    private ProductList boughtProducts;
+
+    ProductMenu productMenu;
+
+    public abstract ProductMenu createProductMenu(int productType);
+    public abstract void showProductMenu();
+
+    public ProductList getEnrolledCourses() {
+        return boughtProducts;
+    }
+
+    public Person(String name, int type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public boolean isBuyer() {
+        return type == 0;
+    }
+
+    public void buyProducts(List<Product> products) {
+
+        if(boughtProducts == null)
+            boughtProducts = new ProductList(products);
+        else
+
+            boughtProducts.addAll(products);
+    }
 
 }
